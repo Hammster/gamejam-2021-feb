582e3a041443960dd201bd02b18d393d02bd2740
Hans Koch <mail@hans-koch.me>
2021-02-13 17:00:52 +0100

    Added battleview, Updated assets

---
+++ Assets/Scripts/ShipAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAnimation : MonoBehaviour
+{
+    public float forceDir = .2f;
+    public float forceRotation = 6f;
+    public float secondsToDir = 10f;
+    public float secondsToRotation = 7f;
+
+    void Awake()
+    {
+        LeanTween.moveLocalY(gameObject, gameObject.transform.position.y - forceDir, secondsToDir).setLoopPingPong();
+        LeanTween.rotateZ(gameObject, gameObject.transform.rotation.z + forceRotation, secondsToRotation).setLoopPingPong();
+    }
+}
---
+++ Assets/Scripts/WaveAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveAnimation : MonoBehaviour
+{
+    public float forceDir;
+    public float secondsOfAnimation = 10;
+
+    void Awake()
+    {
+        LeanTween.moveLocalX(gameObject, gameObject.transform.position.x - forceDir, secondsOfAnimation).setLoopPingPong();
+    }
+}

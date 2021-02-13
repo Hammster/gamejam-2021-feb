df5eed71b0fc67ecdfdf2bc3c2a7d3869c6ad4a9
Dennis Kuehn <dennis.kuehn@udo.edu>
2021-02-13 18:24:11 +0100

    Updated Board movement and animation

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -9,9 +9,9 @@ public class BoardManager : MonoBehaviour
     public float tileSize = 10.0f;
     public GameObject tilePrefab;
     public List<TileScriptableObject> availableTileTypes;
-    private GameObject[,] board;
+    private Tile[,] board;
     private bool pressed = false;
-    
+    private Tile higlightedTile = null;
     public void Awake() {
         CreateBoard("abcabc");
     }
@@ -19,7 +19,7 @@ public class BoardManager : MonoBehaviour
     {
         Random.InitState(seed.GetHashCode());
         // the game board with the lowest left tile being 0,0
-        board = new GameObject[boardWidth,boardHeight];
+        board = new Tile[boardWidth,boardHeight];
         for (int x = 0; x < boardWidth; x++) {
             for (int y = 0; y < boardHeight; y++) {
                 SpawnTile(x, y);
@@ -27,13 +27,31 @@ public class BoardManager : MonoBehaviour
         }
     }
 
+    public void ClickedTile(Tile clicked) {
+        if (higlightedTile == null) {
+            higlightedTile = clicked;
+            // TODO: add highlighting
+        } else {
+            if (!higlightedTile == clicked) {
+                MakeMove(higlightedTile, clicked);
+            }
+            higlightedTile = null;
+        }
+    } 
+
     void SpawnTile(int x, int y) {
         float xPos = x * tileSize;
         float yPos = y * tileSize;
+        float scalingTime = 0.66f; // in seconds
         
         // create the actual tile element in the scene and store it
         GameObject createdTile = MonoBehaviour.Instantiate(tilePrefab, new Vector3(xPos, yPos, 0.0f), Quaternion.identity);
-        board[x, y] = createdTile;
+        createdTile.transform.localScale = new Vector3(0, 0, 0);
+        LeanTween.scale(createdTile, new Vector3(4, 4, 4), scalingTime).setEaseInOutExpo();
+        Tile newTile = createdTile.GetComponent<Tile>();
+        newTile.xIndex = x;
+        newTile.yIndex = y;
+        board[x, y] = newTile;
 
         // set the tile's tile type
         int rndIndex = Random.Range(0, availableTileTypes.Count);
@@ -47,7 +65,7 @@ public class BoardManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.K)) {
             if (!pressed) {
                 pressed = true;
-                CheckBoard();
+                //CheckBoard();
             }
         } else {
             pressed = false;
@@ -67,10 +85,13 @@ public class BoardManager : MonoBehaviour
                 }
                 List<Vector2> matches = FindMatches(x, y);
                 foreach (Vector2 tile in matches) {
-                    allMatches.Add(tile);
+                    if (!allMatches.Contains(tile)) {
+                        allMatches.Add(tile);
+                    }
                 }
-                if (matches.Count > 0)
-                    Debug.Log("added "+matches.Count+" matches for x: "+ x + ", y: "+ y + "(tileType: "+board[x, y].GetComponent<TileController>().tileSource+")");
+                if (matches.Count >0) {
+                    Debug.Log("found "+matches.Count+" matches for tile x:"+x+", y:"+y);
+                }
             }
         }
         
@@ -84,7 +105,7 @@ public class BoardManager : MonoBehaviour
             ApplyGravity();
             
             // add new cells to refill board
-            //AddNewCells();
+            AddNewCells();
         }
         // ensure at least 1 possible match exists
     }
@@ -105,9 +126,8 @@ public class BoardManager : MonoBehaviour
                 if (board[x,y] == null) {
                     for (int above = y+1; above < boardHeight; above++) {
                         if (board[x,above] != null) {
-                            Transform tileTransform = board[x,above].GetComponent<Transform>();
-                            // TODO: lean transform
-                            tileTransform.position = new Vector3(tileTransform.position.x, y * tileSize, tileTransform.position.z);
+                            float animationTime = 0.33f; // in seconds
+                            board[x,above].MoveTile(x, y, animationTime, null);
                             board[x,y] = board[x,above];
                             board[x,above] = null;
                             break;
@@ -123,14 +143,14 @@ public class BoardManager : MonoBehaviour
             int x = (int) tile.x;
             int y = (int) tile.y;
 
-            GameObject tileObject = board[x, y];
-            Destroy(tileObject);
+            Tile tileObject = board[x, y];
+            Destroy(tileObject.gameObject);
             board[x, y] = null;
         }
     }
 
     bool IsMatch(int x, int y, string technicalName, List<Vector2> list) {
-        TileScriptableObject neighbourType = board[x,y].GetComponent<TileController>().tileSource;
+        TileScriptableObject neighbourType = board[x,y].GetTileType();
         //Debug.Log("Neighbour type: "+neighbourType.technicalName +" to "+technicalName);
         if (neighbourType.technicalName.Equals(technicalName)) {
             list.Add(new Vector2(x,y));
@@ -140,10 +160,10 @@ public class BoardManager : MonoBehaviour
     }
 
     List<Vector2> FindMatches(int x, int y) {
-        //Debug.Log("Starting for x: "+ x + ", y: "+ y + "(tileType: "+board[x, y].GetComponent<TileController>().tileSource+")");
+        Debug.Log("Starting for x: "+ x + ", y: "+ y + "(tileType: "+board[x, y].GetTileType()+")");
         List<Vector2> potentialHorizontalMatches = new List<Vector2>();
         List<Vector2> potentialVerticalMatches = new List<Vector2>();
-        TileScriptableObject type = board[x,y].GetComponent<TileController>().tileSource;
+        TileScriptableObject type = board[x,y].GetTileType();
 
         // look horizontally to the left
         for (int i = x-1; i >= 0; i--) {
@@ -190,4 +210,18 @@ public class BoardManager : MonoBehaviour
 
         return actualMatches;
     }
+
+    void MakeMove(Tile firstTile, Tile secondTile) {
+        float animationTime = 0.33f;
+        int firstXIndex = firstTile.xIndex;
+        int firstYIndex = firstTile.yIndex;
+
+        board[firstTile.xIndex, firstTile.yIndex] = secondTile;
+        board[secondTile.xIndex, secondTile.yIndex] = firstTile;
+
+        // animation
+        firstTile.MoveTile(secondTile.xIndex, secondTile.yIndex, animationTime, null);
+        secondTile.MoveTile(firstXIndex, firstYIndex, animationTime, CheckBoard);
+    }
+
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+    public static int TILE_SIZE = 4;
+
+    public BoardManager boardManager;
+
+    public void MoveTile(int xNew, int yNew, float animationTime, System.Action finalAction) {
+        xIndex = xNew;
+        yIndex = yNew;
+        if (finalAction == null) {
+            LeanTween.moveLocal(gameObject, new Vector3(xNew*TILE_SIZE, yNew*TILE_SIZE, 0), animationTime).setEaseInOutExpo();
+        } else {
+            LeanTween.moveLocal(gameObject, new Vector3(xNew*TILE_SIZE, yNew*TILE_SIZE, 0), animationTime).setEaseInOutExpo().setOnComplete(finalAction);
+        }
+    }
+
+    public TileScriptableObject GetTileType() {
+        return gameObject.GetComponent<TileController>().tileSource;
+    }
+
+    void OnMouseDown() {
+        boardManager.ClickedTile(this);
+    }
+}

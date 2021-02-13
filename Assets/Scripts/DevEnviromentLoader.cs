9690c3c3ef70bbfe960bf5676d5f1bc6c24baf90
Hans Koch <mail@hans-koch.me>
2021-02-13 12:32:54 +0100

    Added gamemanager

---
+++ Assets/Scripts/DevEnviromentLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Put this on any object on your scene
+public class DevEnviromentLoader : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject check = GameObject.Find("__app");
+        if (check == null) 
+        { 
+            UnityEngine.SceneManagement.SceneManager.LoadScene("_preload");
+        }
+    }
+}

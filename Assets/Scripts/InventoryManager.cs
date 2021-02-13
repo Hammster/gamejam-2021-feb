3e4ca52220f75c00098eafca917d34b3033bcd72
Tobias Kloss <developer@tobiaskloss.com>
2021-02-13 19:52:13 +0100

    inventory

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    public List<InventorySlot> inventorySlots;
+  
+    public void EnableItem(TileScriptableObject tileScriptableObject)
+    {
+        foreach (var inventorySlot in inventorySlots)
+        {
+            if (inventorySlot.slotType == tileScriptableObject)
+            {
+                inventorySlot.EnableItem();
+            }
+        }
+    }
+    
+    public void DisableItem(TileScriptableObject tileScriptableObject)
+    {
+        foreach (var inventorySlot in inventorySlots)
+        {
+            if (inventorySlot.slotType == tileScriptableObject )
+            {
+                inventorySlot.DisableItem();
+            }
+        }
+    }
+}

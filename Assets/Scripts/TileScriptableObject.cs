799cb68d3a0739c8c0c61c9015639c204089a2a9
Hans Koch <mail@hans-koch.me>
2021-02-13 12:46:32 +0100

    mand

---
+++ Assets/Scripts/TileScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Tile", order = 1)]
+public class TileScriptableObject : ScriptableObject
+{
+    public string technicalName;
+    public Texture2D icon;
+    // visual effect
+    // sound
+}
